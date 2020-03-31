02fa49059047f1e3fc23404b992e13c592ed44da
lelv <imlelv@gmail.com>
2020-03-31 19:59:04 +0200

    business page

--- src/app/business/business.component.ts
+++ src/app/business/business.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BusinessService } from '../services/business.service';
 import { Business } from '../model/business';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-business',
@@ -13,11 +14,24 @@ export class BusinessComponent implements OnInit {
   id: string;
   business: Business;
 
+  chosenProduct: Product;
+  total: number;
+
   constructor(private route: ActivatedRoute, private businessService: BusinessService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
-    this.businessService.getBusiness(this.id).subscribe(response => this.business = response)
+    this.businessService.getBusiness(this.id).subscribe(response => this.business = response);
+  }
+
+  chooseProduct(product: Product) {
+    console.log("product is ", product);
+    this.chosenProduct = product;
+    this.total = product.price;
+  }
+
+  updateTotal(amount: number) {
+    this.total = this.chosenProduct.price * amount;
   }
 
 }
